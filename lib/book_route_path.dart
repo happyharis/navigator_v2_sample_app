78d78b391df7bb3a81e358f260a8b36f719cd8af
Haris <muhamad_haris@hotmail.com>
2020-10-12 15:07:10 +0800

    Add book route path

---
+++ lib/book_route_path.dart
@@ -0,0 +1,18 @@
+class BookRoutePath {
+  final int id;
+  final bool isUnknown;
+
+  BookRoutePath.home()
+      : id = null,
+        isUnknown = false;
+
+  BookRoutePath.details(this.id) : isUnknown = false;
+
+  BookRoutePath.unknown()
+      : id = null,
+        isUnknown = true;
+
+  bool get isHomePage => id == null;
+
+  bool get isDetailsPage => id != null;
+}
