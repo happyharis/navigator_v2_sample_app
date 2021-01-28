2c2fbe9351fb93fc6a20cf1867cdb0091ea76913
Haris Samingan <muhamad_haris@hotmail.com>
2021-01-28 10:37:43 +0800

    Add id

--- lib/book.dart
+++ lib/book.dart
@@ -2,8 +2,9 @@ class Book {
   final String title;
   final String author;
   final String image;
+  final int id;
 
-  Book(this.title, {this.author, this.image});
+  Book(this.title, {this.author, this.image, this.id});
 }
 
 final books = [
@@ -11,30 +12,36 @@ final books = [
     'This Is the Way',
     author: 'Gavin Corbett',
     image: 'images/1.jpg',
+    id: 1,
   ),
   Book(
     'Her',
     author: 'Spike Jonze',
     image: 'images/2.jpg',
+    id: 2,
   ),
   Book(
     'Fight Club',
     author: 'David Fincher',
     image: 'images/3.jpg',
+    id: 3,
   ),
   Book(
     'Enemy',
     author: 'Javier Gullón',
     image: 'images/4.jpg',
+    id: 4,
   ),
   Book(
     'Manual to Minimalism',
     author: 'Unknown',
     image: 'images/5.jpg',
+    id: 5,
   ),
   Book(
     'Dirty Harry',
     author: 'Don Siegel',
     image: 'images/6.jpg',
+    id: 6,
   ),
 ];
